ba1fb7cc6cf7ff2e391ca3c24f72cc133219f670
Mohammed Ibrahim <mohammedi@cpp.edu>
2021-09-29 20:13:27 -0700

    fix random number generator

--- restaurantBill.c
+++ restaurantBill.c
@@ -9,26 +9,32 @@ Outline for Restaurant Bill:
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main()
 {
     // declare necessary variables
-    float taxPercent, tipPercent, billTotal;
+    float tax, tip, billTotal;
     /* create array which holds the prices for each of the meals
                         salad, soup, sandwich, pizza */
     float mealCosts[4] = {9.95, 4.55, 13.25, 22.35};
     // create limit(s) for the random number
     int upperLimit = 3, lowerLimit = 0;
     
+    // different seed for rand function for each execution 
+    srand(time(NULL));
     //generate a random number from 1 to 4
-    int meal = rand()/(RAND_MAX/upperLimit + 1);
-    
+    int meal = rand() % 4;
+    //printf("meal price = %.2f\n", mealCosts[meal]);
+
     // ask user for tax percent
     printf("Enter Tax percent: ");
-    scanf("%2f", &taxPercent);
+    scanf("%2f", &tax);
     // ask user for tip percent
     printf("enter Tip percent: ");
-    scanf("%2f", &tipPercent);
+    scanf("%2f", &tip);
+
+    printf("\nTotal Bill:\n");
 
     return 0;
 }
