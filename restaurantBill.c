90ac44438cd4f9b3a3a7b6502487e358b8595a79
Mohammed Ibrahim <mohammedi@cpp.edu>
2021-09-30 13:59:00 -0700

    remove unneeded variables and commented testing code

--- restaurantBill.c
+++ restaurantBill.c
@@ -11,38 +11,38 @@ Outline for Restaurant Bill:
 #include <stdlib.h>
 #include <time.h>
 
-int main()
+int main(void)
 {
     // declare necessary variables
-    float tax, tip, billTotal;
+    float taxFloat, tipFloat;
+
     /* create array which holds the prices for each of the meals
                         salad, soup, sandwich, pizza */
     float mealCosts[4] = {9.95, 4.55, 13.25, 22.35};
-    
-    // different seed for rand function for each execution 
+
+    // different seed for rand function for each execution
     srand(time(NULL));
     // generate a random number from 0 to 3
-    int meal = rand() % 4;
-    // printf("meal price = %.2f\n", mealCosts[meal]);
+    int mealChoice = rand() % 4;
 
-    // 
-    printf("\nMenu: \n (1) Salad: $%.2f\n (2) Soup: $%.2f\n (3) Sandwich: $%.2f\n (4) Pizza: $%.2f\n\n",
+    // print out the  menu items and their prices
+    printf("\nMenu:\n (1) Salad: $%.2f\n (2) Soup: $%.2f\n (3) Sandwich: $%.2f\n (4) Pizza: $%.2f\n\n",
         mealCosts[0], mealCosts[1], mealCosts[2], mealCosts[3]);
 
-
-    // ask user for tax percent
+    // ask user for tax percentage
     printf("Enter Tax percent: ");
-    scanf("%f", &tax);
-    // ask user for tip percent
-    printf("enter Tip percent: ");
-    scanf("%f", &tip);
+    scanf("%f", &taxFloat);
+    // ask user for tip percentage
+    printf("Enter Tip percent: ");
+    scanf("%f", &tipFloat);
 
     // print out the complete bill to the console
     printf("\n   **Total Bill**\n");
-    printf("Subtotal:\t$%.2f\n", mealCosts[meal]);              // get meal cost
-    printf("Tax:\t\t$%.2f\n", (mealCosts[meal]*(tax/100)));     // calculate tax from meal cost
-    printf("Tip:\t\t$%.2f\n", (mealCosts[meal]*(tip/100)));     // calculate tip from meal cost
-    printf("Total:\t\t$%.2f\n\n", (mealCosts[meal]+(mealCosts[meal]*(tax/100))+(mealCosts[meal]*(tip/100))));   // calculate bill total
+    printf("Subtotal:\t$%.2f\n", mealCosts[mealChoice]);              // get meal price
+    printf("Tax:\t\t$%.2f\n", (mealCosts[mealChoice] * (taxFloat/100)));     // calculate tax from meal cost
+    printf("Tip:\t\t$%.2f\n", (mealCosts[mealChoice] * (tipFloat/100)));     // calculate tip from meal cost
+    printf("Total:\t\t$%.2f\n\n", (mealCosts[mealChoice] + (mealCosts[mealChoice] * (taxFloat/100)) +        // calculate bill total
+                (mealCosts[mealChoice] * (tipFloat/100))));
 
     return 0;
 }
