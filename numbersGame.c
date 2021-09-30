aa97d28f655ee9a26544ab4a5e6dda7a65535d85
Mohammed Ibrahim <mohammedi@cpp.edu>
2021-09-30 16:37:11 -0700

    created outline for number guessing game

---
+++ numbersGame.c
@@ -0,0 +1,14 @@
+/*
+Outline for Number Guessing Game:
+    (1) create the necessary variables
+    (2) Display text to inform the user of the game
+    (3) create a menu for the user to pick from
+    (4) get the users choice/input 
+    (5) create individual functions for each option or use switch case - use some input validation
+        (a) Option 1: get a random number and have the user guess until they guess correctly
+            use looping to continually prompt user to input an integer within the allowed range,
+            also allow user to return to main menu.
+        (b) Option 2: inform user of the current max value and prompt user to enter a new max value
+        (c) Option 3: exit the process
+*/
+
