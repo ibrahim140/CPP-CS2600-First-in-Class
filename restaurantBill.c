ed1d3272c4c4c3cc5f6fc9a2bfd8faf07917c1be
Mohammed Ibrahim <mohammedi@cpp.edu>
2021-09-29 19:09:07 -0700

    declare/initialize variables, get random number and ask user for inputs

--- restaurantBill.c
+++ restaurantBill.c
@@ -6,3 +6,29 @@ Outline for Restaurant Bill:
     4) Perform calculations to get tax amount, tip amount and total bill.
     5) Display bill information in console for user.
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // declare necessary variables
+    float taxPercent, tipPercent, billTotal;
+    /* create array which holds the prices for each of the meals
+                        salad, soup, sandwich, pizza */
+    float mealCosts[4] = {9.95, 4.55, 13.25, 22.35};
+    // create limit(s) for the random number
+    int upperLimit = 3, lowerLimit = 0;
+    
+    //generate a random number from 1 to 4
+    int meal = rand()/(RAND_MAX/upperLimit + 1);
+    
+    // ask user for tax percent
+    printf("Enter Tax percent: ");
+    scanf("%2f", &taxPercent);
+    // ask user for tip percent
+    printf("enter Tip percent: ");
+    scanf("%2f", &tipPercent);
+
+    return 0;
+}
