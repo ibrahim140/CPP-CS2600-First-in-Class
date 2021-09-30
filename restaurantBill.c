d3a5b1aab6ef4414dd44cece52ac8eea892e562b
Mohammed Ibrahim <mohammedi@cpp.edu>
2021-09-29 18:51:44 -0700

    outline for Restaurant Bill project

---
+++ restaurantBill.c
@@ -0,0 +1,8 @@
+/*
+Outline for Restaurant Bill:
+    1) create necessary variablies
+    2) Get a random number from 1 to 4 for picking a random meal - use rand function
+    3) Get  tax percentage, and tip percentage from user.
+    4) Perform calculations to get tax amount, tip amount and total bill.
+    5) Display bill information in console for user.
+*/
